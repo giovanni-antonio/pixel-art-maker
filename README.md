c38eb8b58b85ce8103ac1df052e1ebc05214df2f
Giovanni Lara <hello.giolara@gmail.com>
2018-09-18 14:12:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Pixel Art Maker Project
+
+
+Pixel art maker is a tool/web art editor that allows you to draw pixel art. The project was build with vanillaJS and with a material/minimal UI made with vanilla css.
+
+## Table of Contents
+
+* [About](#About)
+* [Features](#Features)
+* [Instructions](#instructions)
+
+## About
+
+[Pixel art](https://en.wikipedia.org/wiki/Pixel_art) is a form of digital art, created through the use of software, where images are edited on the pixel level. The aesthetic for this kind of graphics comes from 8-bit and 16-bit computers and video game consoles, in addition to other limited systems such as graphing calculators.
+
+Creating or modifying pixel art characters or objects for video games is sometimes called spriting, a term that arose from the hobbyist community.
+
+## Features
+
+- Select canvas size
+- Select Canvas background color
+- Pick paint colors
+- Delete drawing
+
+
+## Instructions
+
+To get started, open `index.html` in your browser and start drawing pixels on the canvas.
+  1. Choose a drawing canvas size and click Save
+  2. Choose a Background Color for the canvas.
+  3. Pick Paint Color.
+  4. Click the squares you would
+  like to paint.
+  5. To erase just double click/tap the square you would like to erase.
+  6. To clear/delete drawing click Delete All (Warning: Drawing progress will not be save)
+  7. HAVE
+  FUN drawing your next masterpiece, cheers!
+
+## TODOs & Feature Request
+  - Save drawing progress
+  - Save drawing
+  - Select pixel sizing
+  - Add a better color picker tool or create one.
+  - Create a random color canvas.
+  - Create a gallery to feature pixel art.
+  - Add social media/sharing
---
+++ app.js
@@ -0,0 +1,69 @@
+const form = document.getElementById('sizePicker'),
+  button = form.querySelector('input[type=submit]'),
+  table = document.getElementById('pixelCanvas');
+const backgroundColorPicker = document.getElementById('pickCanvasColor');
+const Delete = document.getElementById('DeleteCanvas');
+
+function makeGrid() {
+  let inputHeight = form.querySelector('#inputHeight').value,
+    inputWidth = form.querySelector('#inputWidth').value;
+
+  // reset table
+  table.innerHTML = "";
+
+  // create element frame
+  let fragment = document.createDocumentFragment(),
+    tbody = document.createElement("tbody");
+  // loop row and col
+  for (let row = 0; row < inputHeight; row++) {
+    let tr = document.createElement("tr");
+    for (let col = 0; col < inputWidth; col++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+  }
+  fragment.appendChild(tbody);
+
+  // render table elements
+  table.appendChild(fragment);
+}
+
+
+
+function paintCanvas(e) {
+  let color = document.getElementById('colorPicker').value;
+  if (e.target.nodeName == 'TD') {
+    e.target.style.backgroundColor = color;
+  }
+}
+
+function erase(e) {
+  if (e.target.nodeName == 'TD') {
+    e.target.style.backgroundColor = "";
+  }
+}
+
+function DeleteCanvas(e) {
+  e.preventDefault();
+  table.style.backgroundColor = '';
+  let canvasSquares = Array.from(table.querySelectorAll('td'));
+  for (let i = 0; i < canvasSquares.length; i++) {
+    canvasSquares[i].style.backgroundColor = '';
+  }
+}
+
+document.body.addEventListener('onload', makeGrid());
+
+backgroundColorPicker.addEventListener('change', function (e) {
+  table.style.backgroundColor = e.target.value;
+})
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  makeGrid();
+});
+
+table.addEventListener("click", paintCanvas);
+table.addEventListener("dblclick", erase);
+Delete.addEventListener('click', DeleteCanvas);
